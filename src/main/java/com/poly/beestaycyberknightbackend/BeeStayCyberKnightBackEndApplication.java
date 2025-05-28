4d64ec87ba187a34d712203c4188c58a4b1aa95c
Bin-Z123 <binvtps40171@gmail.com>
2025-05-28 08:46:26 +0700

    first commit

---
+++ src/main/java/com/poly/beestaycyberknightbackend/BeeStayCyberKnightBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.poly.beestaycyberknightbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeStayCyberKnightBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeStayCyberKnightBackEndApplication.class, args);
+	}
+
+}
