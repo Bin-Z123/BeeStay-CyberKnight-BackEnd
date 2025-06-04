e1223d57b9987520ab56692c11d21a540d6a2978
PQThinh-052005 <phamquangthinh2152005@gmail.com>
2025-06-04 22:30:08 +0700

    create entity

---
+++ src/main/java/com/poly/beestaycyberknightbackend/domain/entity/Role.java
@@ -0,0 +1,35 @@
+package com.poly.beestaycyberknightbackend.domain.entity;
+
+import java.util.List;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    Long id;    
+
+    @Column(length = 100, nullable = false)
+    String roleName;
+
+    @OneToMany(mappedBy = "role")
+    List<UserRole> users;
+
+}
