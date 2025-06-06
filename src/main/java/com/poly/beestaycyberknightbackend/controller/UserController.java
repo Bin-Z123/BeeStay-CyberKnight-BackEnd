4a0cb44a1dcfa12dfb3f99cf28b8cb9cb8d4ec0b
CHIN <vominhtriet031105@gmail.com>
2025-06-07 03:39:33 +0700

    hash passwordEncoder

--- src/main/java/com/poly/beestaycyberknightbackend/controller/UserController.java
+++ src/main/java/com/poly/beestaycyberknightbackend/controller/UserController.java
@@ -11,13 +11,12 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -26,26 +25,24 @@ import org.springframework.web.bind.annotation.PutMapping;
 public class UserController {
     
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
-
-    // @PostMapping("/users")
-    // public ResponseEntity<User> createNewUser(@RequestBody User postManUser) {
-    //     String hashPassword = this.passwordEncoder.encode(postManUser.getPassword());
-    //     postManUser.setPassword(hashPassword);
-    //     User user = this.userService.handleCreateUser(postManUser);
-    //     return ResponseEntity.status(HttpStatus.CREATED).body(user);
-    // }
 
     @PostMapping("/users")
     public ResponseEntity<User> createNewUser(@RequestBody User postManUser) {
-        User newUser = this.userService.handleCreateUser(postManUser);
-        return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
+        String hashPassword = this.passwordEncoder.encode(postManUser.getPassword());
+        postManUser.setPassword(hashPassword);
+        User user = this.userService.handleCreateUser(postManUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
+
     
-    @DeleteMapping("/user/{id}")
+    
+    @DeleteMapping("/userss/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") long id) throws IdInvalidException {
         if (id >= 10000) {
             throw new IdInvalidException("Id không lớn hơn 10000");
@@ -54,7 +51,7 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body("Success");
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/users/{id}")
     public ResponseEntity<User> getUserById(@PathVariable("id") long id) {
         User fetchUser = this.userService.fetchUserById(id);
         return ResponseEntity.status(HttpStatus.OK).body(fetchUser);
@@ -65,5 +62,8 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.fetchAllUser());
     }
 
-
+    @GetMapping("/")
+    public String home(){
+        return "hello";
+    }
 }
