faf6f8b0084b4a131284b683d9f26780c9863572
PQThinh-052005 <phamquangthinh2152005@gmail.com>
2025-06-30 17:42:49 +0700

    fix: apiresponse for controller (#37)

--- src/main/java/com/poly/beestaycyberknightbackend/controller/admin/RoomController.java
+++ src/main/java/com/poly/beestaycyberknightbackend/controller/admin/RoomController.java
@@ -2,6 +2,7 @@ package com.poly.beestaycyberknightbackend.controller.admin;
 
 import java.util.List;
 
+import com.cloudinary.Api;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.poly.beestaycyberknightbackend.dto.request.RoomUpdateRequest;
@@ -30,34 +31,34 @@ public class RoomController {
     private final RoomService roomService;
 
     @PostMapping(value = "/rooms",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<RoomResponse> createNewRoom(@RequestPart(name = "rooms") RoomRequest roomRequest, @RequestPart(name = "file", required = false)List<MultipartFile> multipartFiles) throws JsonProcessingException {
+    public ApiResponse<RoomResponse> createNewRoom(@RequestPart(name = "rooms") RoomRequest roomRequest, @RequestPart(name = "file", required = false)List<MultipartFile> multipartFiles) throws JsonProcessingException {
         System.out.println("rooms: " + roomRequest); // xem JSON tới chưa
         System.out.println("files: " + multipartFiles.size());
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(roomService.handleCreateRoom(roomRequest, multipartFiles));
+        return new ApiResponse<>(HttpStatus.CREATED.value(), null, roomService.handleCreateRoom(roomRequest, multipartFiles));
     }
 
     @DeleteMapping("/rooms/{id}")
-    public ResponseEntity<Void> deleteRoom(@PathVariable("id") long id) {
+    public ApiResponse<Void> deleteRoom(@PathVariable("id") long id) {
         roomService.fetchRoomById(id);
         roomService.handleDeleteRoom(id);
-        return ResponseEntity.noContent().build();
+        return new ApiResponse<>(HttpStatus.NO_CONTENT.value(), null, null);
     }
 
     @GetMapping("/rooms")
-    public ResponseEntity<List<RoomResponse>> getAllRoom() {
-        return ResponseEntity.status(HttpStatus.OK).body(roomService.fetchAllRooms());
+    public ApiResponse<List<RoomResponse>> getAllRoom() {
+        return new ApiResponse<>(200,null,roomService.fetchAllRooms());
     }
 
     @GetMapping("/rooms/{id}")
-    public ResponseEntity<RoomResponse> getRoomById(@PathVariable("id") long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(roomService.fetchRoomById(id));
+    public ApiResponse<RoomResponse> getRoomById(@PathVariable("id") long id) {
+        return new ApiResponse<>(200,null,roomService.fetchRoomById(id));
     }
     @PutMapping(value = "/rooms/{id}",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<RoomResponse> updateRoom(@PathVariable("id") long  id, @RequestPart(name = "rooms") RoomUpdateRequest roomUpdateRequest, @RequestPart(name = "file", required = false)List<MultipartFile> multipartFiles) {
+    public ApiResponse<RoomResponse> updateRoom(@PathVariable("id") long  id, @RequestPart(name = "rooms") RoomUpdateRequest roomUpdateRequest, @RequestPart(name = "file", required = false)List<MultipartFile> multipartFiles) {
         System.out.println("rooms: " + roomUpdateRequest);
         System.out.println("room id: " + id);
-        return ResponseEntity.status(HttpStatus.CREATED).body(roomService.handleUpdateRoom(roomUpdateRequest,id, multipartFiles));
+        return new ApiResponse<>(200,null,roomService.handleUpdateRoom(roomUpdateRequest,id, multipartFiles));
     }
 
     @GetMapping("/roomsinactive")
