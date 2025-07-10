de8b231835dfb0bdd167eaa240638bd31298c691
PQThinh-052005 <phamquangthinh2152005@gmail.com>
2025-07-10 15:00:43 +0700

    fea: add list stay and list booking into list room

--- src/main/java/com/poly/beestaycyberknightbackend/repository/StayRepository.java
+++ src/main/java/com/poly/beestaycyberknightbackend/repository/StayRepository.java
@@ -1,6 +1,7 @@
 package com.poly.beestaycyberknightbackend.repository;
 
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,4 +15,12 @@ import com.poly.beestaycyberknightbackend.domain.Stay;
 public interface StayRepository extends JpaRepository<Stay, Integer> {
     @Query("SELECT s FROM Stay s WHERE s.booking.id = :bId")
     List<Stay> listStayOfBooking(@Param("bId") Long bId);
+
+    @Query(value = """
+            DECLARE @today DATETIME = :today
+            SELECT s.* FROM Rooms r JOIN Stays s on r.id = s.room_id
+		   WHERE s.actualcheckin <= @today AND @today <= s.actualcheckout
+				 AND r.id = :roomId
+            """, nativeQuery = true)
+    List<Stay> findStayByRoomId(Long roomId, LocalDateTime today);
 }
