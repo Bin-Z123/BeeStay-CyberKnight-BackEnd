048a2c71b2a1dc6c666ea6a3de6fb2f34a23430b
PQThinh-052005 <phamquangthinh2152005@gmail.com>
2025-07-21 14:34:26 +0700

    Api calculates total payment.getStatus(PAID) of booking

--- src/main/java/com/poly/beestaycyberknightbackend/controller/admin/PaymentController.java
+++ src/main/java/com/poly/beestaycyberknightbackend/controller/admin/PaymentController.java
@@ -6,12 +6,18 @@ import com.poly.beestaycyberknightbackend.domain.Payment;
 import com.poly.beestaycyberknightbackend.dto.request.PaymentByCashRequest;
 import com.poly.beestaycyberknightbackend.dto.response.ApiResponse;
 import com.poly.beestaycyberknightbackend.service.PaymentByCashService;
+import com.poly.beestaycyberknightbackend.service.PaymentService;
+
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.apache.hc.core5.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -19,11 +25,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/api/admin/payment")
 public class PaymentController {
     PaymentByCashService paymentByCastService;
+    PaymentService paymentService;
 
 
     @PostMapping("/pay")
     public ApiResponse<Payment> createPaymentByCast(@RequestBody PaymentByCashRequest request) {
         return new ApiResponse<>(HttpStatus.SC_OK, null, paymentByCastService.createPaymentByCast(request));
     }
+
+    @GetMapping("/calculatePaymentofBooking/{bookingId}")
+    public ApiResponse<Integer> calculatePaymentofBooking(@PathVariable Long bookingId) {
+        return new ApiResponse<>(HttpStatus.SC_OK, null, paymentService.sumPaymentPAIDOfBooking(bookingId));
+    }
+    
     
 }
