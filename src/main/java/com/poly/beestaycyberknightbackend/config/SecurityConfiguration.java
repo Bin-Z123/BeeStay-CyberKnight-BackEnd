f3d49a631c8b64944190a5759bc9c608b9cb16a9
PQThinh-052005 <phamquangthinh2152005@gmail.com>
2025-07-25 16:43:17 +0700

    Fix/return url (#82)
    * cấu hình return url và cancel url

--- src/main/java/com/poly/beestaycyberknightbackend/config/SecurityConfiguration.java
+++ src/main/java/com/poly/beestaycyberknightbackend/config/SecurityConfiguration.java
@@ -109,10 +109,11 @@ public class SecurityConfiguration {
         return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
     }
 
+    
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(List.of("http://localhost:5173"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
         corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
