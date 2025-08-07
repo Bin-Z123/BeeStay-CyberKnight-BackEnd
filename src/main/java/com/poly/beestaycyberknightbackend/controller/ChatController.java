0609d66ea99233d8fbf65cf02559b571b4d6dfa4
Bin-Z123 <binvtps40171@gmail.com>
2025-08-07 11:04:42 +0700

    add chatMessage DTO

--- src/main/java/com/poly/beestaycyberknightbackend/controller/ChatController.java
+++ src/main/java/com/poly/beestaycyberknightbackend/controller/ChatController.java
@@ -7,11 +7,14 @@ import java.util.Map;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 // import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.poly.beestaycyberknightbackend.domain.User;
+import com.poly.beestaycyberknightbackend.dto.ChatDTO;
+import com.poly.beestaycyberknightbackend.dto.ChatDTO.MessageType;
 import com.poly.beestaycyberknightbackend.repository.UserRepository;
 import com.poly.beestaycyberknightbackend.util.SecurityUtil;
 
@@ -21,21 +24,60 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 // @RequestMapping("/api")
 public class ChatController {
-    private final SecurityUtil securityUtil;
+    // private final SecurityUtil securityUtil;
     private final UserRepository userRepository;
 
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public String send(Principal principal, String message) {
-
+    public ChatDTO send(Principal principal, @Payload String message) {
         if (principal == null) {
-            return "User Person: " + message;
+            return ChatDTO.builder()
+                    .content("Không thể xác thực người dùng!!")
+                    .type(MessageType.LEAVE)
+                    .build();
         }
+
         String email = principal.getName();
         User user = userRepository.findByEmail(email);
 
-        String sender = (user != null) ? user.getFullname() : user.getEmail();
+        return ChatDTO.builder()
+                .content(message)
+                .timestamp(System.currentTimeMillis())
+                .type(MessageType.CHAT)
+                .senderId(user.getId()) // Giả sử User có getId()
+                .senderFullName(user.getFullname())
+                .build();
+        // if (principal == null) {
+        // return "User Person: " + message;
+        // }
+        // String email = principal.getName();
+        // User user = userRepository.findByEmail(email);
 
-        return sender + ": " + message;
+        // String sender = (user != null) ? user.getFullname() : user.getEmail();
+
+        // return sender + ": " + message;
+    }
+
+    @MessageMapping("/join")
+    @SendTo("/topic/messages")
+    public ChatDTO join(Principal principal, @Payload String message) {
+
+        if (principal == null) {
+            return ChatDTO.builder()
+                    .content("Không thể xác thực người dùng!!")
+                    .type(MessageType.LEAVE)
+                    .build();
+        }
+
+        String email = principal.getName();
+        User user = userRepository.findByEmail(email);
+
+        return ChatDTO.builder()
+                .content(message)
+                .timestamp(System.currentTimeMillis())
+                .type(MessageType.JOIN)
+                .senderId(user.getId()) // Giả sử User có getId()
+                .senderFullName(user.getFullname())
+                .build();
     }
 }
---
+++ src/main/java/com/poly/beestaycyberknightbackend/dto/ChatDTO.java
@@ -0,0 +1,25 @@
+package com.poly.beestaycyberknightbackend.dto;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ChatDTO {
+    private String content;
+    private long timestamp;
+    private MessageType type;
+
+    // Thông tin người gửi
+    private long senderId;
+    private String senderFullName;
+
+    public enum MessageType {
+        CHAT,
+        JOIN,
+        LEAVE
+    }
+}
